5a4aacea4c22a6ee3707dd73a8091aabf9eff762
valentine <ugwuvalentine519@gmail.com>
2024-07-04 00:56:01 +0100

    small changes

--- src/App.js
+++ src/App.js
@@ -84,10 +84,26 @@ const Form = ({ onAdditems }) => {
 };
 
 const PackingList = ({ item, onDelete, oncheckbox }) => {
+  const [sortBy, setsortBy] = useState('input');
+
+  let sorteditems;
+
+  if (sortBy === 'input') sorteditems = item;
+
+  if (sortBy === 'description')
+    sorteditems = item
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sortBy === 'packed')
+    sorteditems = item
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+
   return (
     <div className="list">
       <ul>
-        {item.map((item) => (
+        {sorteditems.map((item) => (
           <Item
             item={item}
             onDelete={onDelete}
@@ -96,6 +112,14 @@ const PackingList = ({ item, onDelete, oncheckbox }) => {
           />
         ))}
       </ul>
+
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setsortBy(e.target.value)}>
+          <option value="iput">sort by input order </option>
+          <option value="description">sort by description</option>
+          <option value="packed">sort by packed</option>
+        </select>
+      </div>
     </div>
   );
 };
